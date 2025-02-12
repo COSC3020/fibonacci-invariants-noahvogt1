924f4c357d2959baca7ea18f721c7594fa949e79
noahvogt1 <96090888+noahvogt1@users.noreply.github.com>
2025-02-11 20:29:52 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,7 +1,7 @@
-function fib(n) {
-  if (n <= 1) {
-    return n;
-  } else {
-    return fib(n - 1) + fib(n - 2);
+function fib(n, arr = [0,1]) {
+  if (n <= arr.length) {
+    return arr.slice(0, n + 1;
   }
+  arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+  return fib(n, arr);
 }
